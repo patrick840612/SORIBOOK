05fa11169946d24be5105da1d99f09f7d593155d
blueberry <jungmin8205@naver.com>
2023-05-03 15:04:04 +0900

    정민

---
+++ SoriBook/src/main/java/com/kosmo/soribook/domain/MajorCategoryVO.java
@@ -0,0 +1,13 @@
+package com.kosmo.soribook.domain;
+
+import lombok.Data;
+ 
+@Data
+public class MajorCategoryVO {  	   // 카테고리 대분류 클래스
+	private String MajorCategoryNo;    // 대분류 번호
+	private String MajorCategoryName;  // 대분류 이름
+	
+
+	
+
+}
